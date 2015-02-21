d34dc4a08fe88a238f273817a372f6fa0334eebb
dishwishy <k.champlin@gmail.com>
2015-02-20 17:28:30 -0800

    new stuff

--- scraperTests.py
+++ scraperTests.py
@@ -3,10 +3,7 @@ __author__ = 'kylechamplin'
 import requests
 from BeautifulSoup import BeautifulSoup
 
-
-
-
-r = requests.get("http://seattle.craigslist.org/search/sss?excats=7-13-22-2-24-1-4-19-1-1-1-1-1-1-3-6-10-1-1-1-2-2-8-1-2-2-4-1-3-1-3-1-1-1-1-7-1-1-1-1-1-1-1-1-1-1-1-1-1-2-1-1-1-2-1-1-2-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1-1&sort=rel&query=cabinets")
+r = requests.get("http://seattle.craigslist.org/search/sss?query=cabinet%2030%20inches&sort=rel")
 soup = BeautifulSoup(r.text)
 results = soup.findAll("a", {"class" :"hdrlnk"})
 prices = soup.findAll("span", {"class" : "price"})
